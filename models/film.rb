709e092759cd32db0467ece68f407344651b713b
Gmorgan87 <gmorgan87@hotmail.co.uk>
2020-01-31 13:17:56 +0000

    adds update and delete methods to film

--- models/film.rb
+++ models/film.rb
@@ -22,6 +22,18 @@ def save
   @id = results[0]['id'].to_i
 end
 
+def update
+  sql = "UPDATE films SET (title,price) = ($1,$2) WHERE id = $3"
+  values = [@title, @price, @id]
+  SqlRunner.run(sql, values)
+end
+
+def delete
+  sql = "DELETE FROM films WHERE id = $1"
+  values = [@id]
+  SqlRunner.run(sql, values)
+end
+
 def self.all
   sql = "SELECT * FROM films"
   results = SqlRunner.run(sql)
