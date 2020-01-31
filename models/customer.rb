29f90ae7d4fb7281b25876907a8556e9966c9480
Gmorgan87 <gmorgan87@hotmail.co.uk>
2020-01-31 13:08:09 +0000

    adds save method to all models

--- models/customer.rb
+++ models/customer.rb
@@ -3,13 +3,25 @@ require_relative('../db/sql_runner')
 class Customer
 
   attr_reader :id
-  attr_accesser :name, :funds
+  attr_accessor :name, :funds
 
   def initialize(options)
     @id = options['id'].to_i if options['id']
     @name= options['name']
-    @funds = options['funds']to_i
+    @funds = options['funds'].to_i
   end
 
+  def save
+    sql = "INSERT INTO customers
+    (name, funds)
+    VALUES
+    ($1,$2)
+    RETURNING id"
+    values = [@name, @funds]
+    results = SqlRunner.run(sql, values)
+    @id = results[0]['id'].to_i
+  end
+
+  
 
 end
--- models/film.rb
+++ models/film.rb
@@ -3,12 +3,29 @@ require_relative('../db/sql_runner')
 class Film
 
 attr_reader :id
-attr_accesser :title, :price
+attr_accessor :title, :price
 
 def initialize(options)
-  @id = options['id']to_i if options['id']
+  @id = options['id'].to_i if options['id']
   @title = options['title']
   @price = options['price'].to_i
+end
+
+def save
+  sql = "INSERT INTO films
+  (title, price)
+  VALUES
+  ($1,$2)
+  RETURNING id"
+  values = [@title, @price]
+  results = SqlRunner.run(sql, values)
+  @id = results[0]['id'].to_i
+end
+
+def self.all
+  sql = "SELECT * FROM films"
+  results = SqlRunner.run(sql, values)
+
 end
 
 end
